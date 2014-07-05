875f84d6015d37dd7fbd18061fd733f123662806
Nealon Young <hi@nealyoung.me>
2014-07-04 17:43:12 -0700

    Added support for multi-line reminders

--- LockMinder/ImageGenerator.m
+++ LockMinder/ImageGenerator.m
@@ -9,14 +9,14 @@
 #import <EventKit/EventKit.h>
 #import "ImageGenerator.h"
 
-@implementation ImageGenerator
-
 static CGFloat const kClockHeight = 160.0f;
 static CGFloat const kSliderHeight = 90.0f;
-//static CGFloat const kListHeaderHeight = 24.0f;
-static CGFloat const kListInset = 20.0f;
+static CGFloat const kListBackgroundInset = 20.0f;
 static CGFloat const kListItemXInset = 15.0f;
-static CGFloat const kListItemHeight = 25.0f;
+static CGFloat const kListItemPadding = 2.0f;
+static CGFloat const kItemBulletWidth = 5.0f;
+
+@implementation ImageGenerator
 
 + (UIImage *)wallpaperImageWithBackground:(UIImage *)backgroundImage reminders:(NSArray *)reminders {
     CGRect screenBounds = [UIScreen mainScreen].bounds;
@@ -26,7 +26,17 @@ static CGFloat const kListItemHeight = 25.0f;
     
     [backgroundImage drawInRect:[UIScreen mainScreen].bounds];
     
-    CGFloat listBackgroundHeight = kListItemXInset * 2.0f + kListItemHeight * [reminders count];
+    CGFloat listBackgroundHeight = kListItemXInset * 2.0f;
+
+    for (EKReminder *reminder in reminders) {
+        CGRect reminderRect = [reminder.title boundingRectWithSize:CGSizeMake(screenBounds.size.width - (kListBackgroundInset * 2.0f) - (kListItemXInset * 2.0f), 9999.0f)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:17.0f]}
+                                                           context:nil];
+        reminderRect.size.height += kListItemPadding * 2.0f;
+        listBackgroundHeight += CGRectGetHeight(reminderRect);
+    }
+    
     
     // Make sure the list background doesn't extend over the unlock slider
     CGFloat maxListHeight = screenBounds.size.height - kClockHeight - kSliderHeight;
@@ -38,9 +48,9 @@ static CGFloat const kListItemHeight = 25.0f;
     CGFloat listVerticalOffset = (maxListHeight - listBackgroundHeight) / 2.0f;
     
     // Determine the size of the overlay
-    CGRect listBackgroundRect = CGRectMake(kListInset,
+    CGRect listBackgroundRect = CGRectMake(kListBackgroundInset,
                                            kClockHeight + listVerticalOffset,
-                                           screenBounds.size.width - kListInset * 2.0f,
+                                           screenBounds.size.width - kListBackgroundInset * 2.0f,
                                            listBackgroundHeight);
     UIBezierPath *listBackgroundPath = [UIBezierPath bezierPathWithRoundedRect:listBackgroundRect
                                                              byRoundingCorners:UIRectCornerAllCorners
@@ -49,33 +59,41 @@ static CGFloat const kListItemHeight = 25.0f;
     CGContextSetFillColorWithColor(ctx, [[UIColor colorWithWhite:1.0f alpha:0.8f] CGColor]);
     CGContextFillPath(ctx);
     
-//    CGRect listHeaderRect = CGRectMake(CGRectGetMinX(listBackgroundRect) + 15.0f,
-//                                       CGRectGetMinY(listBackgroundRect) + 2.0f,
-//                                       CGRectGetWidth(listBackgroundRect) - 30.0f,
-//                                       kListHeaderHeight);
-//    
-//    // Center the header text
-//    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-//    [style setAlignment:NSTextAlignmentCenter];
-//    [@"Reminders" drawInRect:listHeaderRect withAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Medium" size:17.0f],
-//                                                          NSParagraphStyleAttributeName: style}];
+    __block CGFloat yOffset = 0.0f;
     
-    [reminders enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        EKReminder *reminder = (EKReminder *)obj;
-        CGRect listItemRect = CGRectMake(CGRectGetMinX(listBackgroundRect) + kListItemXInset,
-                                         CGRectGetMinY(listBackgroundRect) + kListItemXInset + (kListItemHeight * idx),
-                                         CGRectGetWidth(listBackgroundRect) - 30.0f,
-                                         20.0f);
-        [[NSString stringWithFormat:@"· %@", reminder.title] drawInRect:listItemRect
-                                                         withAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:17.0f]}];
+    [reminders enumerateObjectsUsingBlock:^(EKReminder *reminder, NSUInteger idx, BOOL *stop) {
+        // Draw the bullet point
+        CGRect itemBulletRect = CGRectMake(CGRectGetMinX(listBackgroundRect) + kListItemXInset,
+                                           CGRectGetMinY(listBackgroundRect) + kListItemXInset + yOffset + kItemBulletWidth * 1.5,
+                                           kItemBulletWidth,
+                                           kItemBulletWidth);
+        UIBezierPath *itemBulletPath = [UIBezierPath bezierPathWithOvalInRect:itemBulletRect];
+        CGContextBeginPath(ctx);
+        CGContextAddPath(ctx, itemBulletPath.CGPath);
+        CGContextSetFillColorWithColor(ctx, [UIColor blackColor].CGColor);
+        CGContextFillPath(ctx);
+        
+        // Compute the size required to display the reminder (to support multi-line text)
+        CGRect reminderRect = [reminder.title boundingRectWithSize:CGSizeMake(screenBounds.size.width - (kListBackgroundInset * 2.0f) - (kListItemXInset * 2.0f), 9999.0f)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:17.0f]}
+                                                           context:nil];
+        reminderRect.origin = CGPointMake(CGRectGetMinX(listBackgroundRect) + kListItemXInset + (kItemBulletWidth * 2.0f),
+                                          CGRectGetMinY(listBackgroundRect) + kListItemXInset + yOffset);
+        reminderRect.size.height += kListItemPadding * 2.0f;
+
+        
+        yOffset += CGRectGetHeight(reminderRect);
+        
+        [reminder.title drawInRect:reminderRect
+                    withAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:17.0f]}];
     }];
     
-    // Create the UIImage to save to the user's photo library
     CGImageRef img = CGBitmapContextCreateImage(ctx);
+    UIGraphicsEndImageContext();
+
     UIImage *wallpaperImage = [UIImage imageWithCGImage:img];
     CGImageRelease(img);
-    
-    UIGraphicsEndImageContext();
     
     return wallpaperImage;
 }
